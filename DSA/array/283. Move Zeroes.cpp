a63bfda34b83571c3ed2a094b54dfdaa377e7b5f
DARSHAN J PATEL <102922350+DJp119@users.noreply.github.com>
2022-07-22 23:22:34 +0530

    Create 283. Move Zeroes.cpp
    https://leetcode.com/problems/move-zeroes/

---
+++ DSA/array/283. Move Zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0 ;  // non-zero value
+        for(int j = 0 ;j<nums.size();j++){
+            if(nums[j] != 0){
+                swap(nums[j],nums[i]);
+                i++;
+            }
+        }
+    }
+};
