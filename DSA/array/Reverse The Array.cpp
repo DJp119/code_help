d7de2b75cd642a8de3ea944134c3d83f3b506004
DARSHAN J PATEL <102922350+DJp119@users.noreply.github.com>
2022-07-22 21:58:21 +0530

    Create Reverse The Array.cpp
    https://www.codingninjas.com/codestudio/problems/reverse-the-array_1262298?leftPanelTab=0

---
+++ DSA/array/Reverse The Array.cpp
@@ -0,0 +1,14 @@
+void reverseArray(vector<int> &arr , int m)
+{
+    int s = m+1 ,  e = arr.size()-1;
+    while (s<=e)
+    {
+        swap(arr[s],arr[e]);
+        s++;
+        e--;
+
+        /* code */
+    }
+    
+	// Write your code here.
+}
