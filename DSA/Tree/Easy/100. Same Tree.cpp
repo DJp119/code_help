b15b10df48a8d759862f5972536d5aac6eaf52ff
DARSHAN J PATEL <102922350+DJp119@users.noreply.github.com>
2023-05-09 19:35:34 +0530

    Update 100. Same Tree.cpp

--- DSA/Tree/Easy/100. Same Tree.cpp
+++ DSA/Tree/Easy/100. Same Tree.cpp
@@ -18,3 +18,20 @@ public:
         return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
+class Solution
+{
+    public:
+    //Function to check if two trees are identical.
+    bool isSameTree(TreeNode *r1, TreeNode *r2)
+    {
+        if(!r1 && !r2) return true;
+        if(!r1 && r2) return false;
+        if(r1 && !r2) return false;
+        
+        bool left = isSameTree(r1->left,r2->left);
+        bool right = isSameTree(r1->right,r2->right);
+        bool val = r1->val == r2->val;
+        if(left && right && val) return true;
+        else return false;
+    }
+};
